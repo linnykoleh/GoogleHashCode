e1de1a0fa5549f366d00cee9d280607abed749e1
Solvegam <solvegam2011@yandex.ua>
2017-02-23 22:08:56 +0200

    + Endpoints parse

--- src/main/java/processor/MainProcessor.java
+++ src/main/java/processor/MainProcessor.java
@@ -3,18 +3,18 @@ package processor;
 import provider.EndpointProvider;
 import provider.RequestsProvider;
 import structure.EnterDataInfo;
-import provider.VideoProvider;
+import provider.DataCenter;
 
 public class MainProcessor {
 
     private EnterDataInfo info;
-    private VideoProvider videoProvider;
+    private DataCenter dataCenter;
     private EndpointProvider endpointProvider;
     private RequestsProvider requestsProvider;
 
-    public MainProcessor(EnterDataInfo info, VideoProvider videoProvider, EndpointProvider endpointProvider, RequestsProvider requestsProvider) {
+    public MainProcessor(EnterDataInfo info, DataCenter dataCenter, EndpointProvider endpointProvider, RequestsProvider requestsProvider) {
         this.info = info;
-        this.videoProvider = videoProvider;
+        this.dataCenter = dataCenter;
         this.endpointProvider = endpointProvider;
         this.requestsProvider = requestsProvider;
     }
