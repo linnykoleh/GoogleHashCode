e6188db545707c96a12381dc93d3c67e95f6c4dc
Solvegam <solvegam2011@yandex.ua>
2017-02-23 23:15:34 +0200

    EndPointWithID

--- src/main/java/structure/Cache.java
+++ src/main/java/structure/Cache.java
@@ -1,10 +1,14 @@
 package structure;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cache {
 
     private int cacheId;
     private int latencyTime;
     private int size;
+    private List<Video> videos = new ArrayList<>();
 
     public Cache(int cacheId, int latencyTime, int size) {
         this.cacheId = cacheId;
@@ -22,5 +26,9 @@ public class Cache {
 
     public int getAvelibleSize() {
         return size;
+    }
+
+    public void addVideo(Video video) {
+        videos.add(video);
     }
 }
