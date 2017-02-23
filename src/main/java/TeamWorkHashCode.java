f1c87e481b97bdceb07447c34368be2529dcddab
Solvegam <solvegam2011@yandex.ua>
2017-02-23 21:05:02 +0200

    + Requests parse

--- src/main/java/TeamWorkHashCode.java
+++ src/main/java/TeamWorkHashCode.java
@@ -1,4 +1,6 @@
+import structure.Endpoint;
 import structure.EnterDataInfo;
+import structure.Request;
 import structure.Video;
 
 import java.nio.file.Files;
@@ -12,6 +14,8 @@ public class TeamWorkHashCode {
     private static EnterDataInfo enterDataInfo;
 
     private static List<Video> videos;
+    private static List<Endpoint> endpoints;
+    private static List<Request> requests;
 
     public static void main(String[] args) throws Exception {
         final String file = defineFileName();
@@ -58,5 +62,30 @@ public class TeamWorkHashCode {
             videos.add(new Video(i, Integer.parseInt(videosStrings[i])));
         }
 
+        requests = parseRequests(strings);
+
+
+//        endpoints = parseEndPoints(strings);
+
+    }
+
+    private static List<Endpoint> parseEndPoints(List<String> strings) {
+        return null;
+    }
+
+    private static List<Request> parseRequests(List<String> strings) {
+        List<String> requestsStrings =
+                strings.subList(strings.size() - enterDataInfo.getRequestDescriptions(), strings.size());
+
+        List<Request> requests = new ArrayList<>();
+
+        requestsStrings.forEach(requestString -> {
+            String[] requestStringParams = requestString.split(" ");
+            requests.add(new Request(Integer.parseInt(requestStringParams[0]),
+                    Integer.parseInt(requestStringParams[1]),
+                    Integer.parseInt(requestStringParams[2])));
+        });
+
+        return requests;
     }
 }
