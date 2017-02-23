184e4faba7d25aa4a55aa5e9d27515589f993785
LinnykOleh <linnik.oleg.93@gmail.com>
2017-02-23 21:02:23 +0200

    renamed provider and main processor

---
+++ src/main/java/processor/MainProcessor.java
@@ -0,0 +1,11 @@
+package processor;
+
+import structure.EnterDataInfo;
+import provider.VideoProvider;
+
+public class MainProcessor {
+
+    private EnterDataInfo info;
+    private VideoProvider videoProvider;
+
+}
---
+++ src/main/java/provider/VideoProvider.java
@@ -0,0 +1,21 @@
+package provider;
+
+import structure.Video;
+
+import java.util.List;
+
+/**
+ * Class for working with video
+ */
+public class VideoProvider {
+
+    private List<Video> videos;
+
+    public VideoProvider(List<Video> videos) {
+        this.videos = videos;
+    }
+
+    public void findSomethind(){
+
+    }
+}
