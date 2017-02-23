6ecb8850f81438b1f738a3d622c7c24cac5e2642
LinnykOleh <linnik.oleg.93@gmail.com>
2017-02-23 20:28:57 +0200

    added video

---
+++ src/main/java/structure/Video.java
@@ -0,0 +1,20 @@
+package structure;
+
+public class Video {
+
+    private int id;
+    private int size;
+
+    public Video(int id, int size) {
+        this.id = id;
+        this.size = size;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
